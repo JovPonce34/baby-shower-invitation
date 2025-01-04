3d7ef6c85865387af120b38244b10d6a1387973b
Jovani Ponce <jovponce34@gmail.com>
2025-01-03 20:40:26 -0600

    Se corrige la imagen de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import backgroundImage from '../assets/header.png';
+
 const HeaderContainer = styled(Box)(({ theme }) => ({
     padding: theme.spacing(10),
     marginBottom: theme.spacing(2),
-    backgroundImage: `url('../src/assets/header.png')`,
+    backgroundImage: `url(${backgroundImage})`,
     backgroundSize: 'contain',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
