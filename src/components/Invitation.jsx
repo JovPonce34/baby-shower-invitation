46b9a0b342b5dd40391ca8f6cf8a22224561cb36
Jovani Ponce <jovponce34@gmail.com>
2025-01-03 20:26:28 -0600

    Se agregan fuentes para el componente Invitacion

--- src/components/Invitation.jsx
+++ src/components/Invitation.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Paper, Typography, Box } from '@mui/material';
+import { Paper, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
@@ -14,19 +13,29 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 function Invitation() {
     return (
         <StyledPaper elevation={3}>
-            <Typography variant="h5" color="secondary" gutterBottom>
-            Con mucha alegría te invitamos al Baby Shower de
-            </Typography>
-            <Typography variant="h4" color="primary" sx={{ fontWeight: 'bold', my: 2 }}>
-            [Nombre del Bebé]
-            </Typography>
-            <Typography variant="body1" paragraph>
-            Acompáñanos en este día tan especial donde celebraremos la próxima llegada de nuestro pequeño aventurero.
-            </Typography>
-            <Box sx={{ my: 3 }}>
-            <Typography variant="h6">Fecha: [Fecha del evento]</Typography>
-            <Typography variant="h6">Hora: [Hora del evento]</Typography>
+            
+            <h1 className='dancing-script-encabezado'>
+                Un nuevo rayito de sol llega a nuestras vidas...
+            </h1>
+            
+            <h1 className='allura-regular'>
+            Liam Mateo
+            </h1>
+            <h2 className='great-vibes-regular'>
+                Con mucha emoción, les invitamos a celebrar la llegada de nuestro pequeño tesoro.
+            </h2>
+            <h3 className='lora'>
+                El amor está por nacer y queremos compartirlo con ustedes. Acompáñanos a celebrar este maravilloso momento en el que la vida se llena de nuevas ilusiones y sonrisas.
+            </h3>
+            
+            <Box  sx={{ my: 3 }}>
+            <h2 className='raleway'>Fecha: 15 de Febreo 2025</h2>
+            <h2 className='raleway'>Hora: 4:00 PM</h2>
             </Box>
+            <h3 className='nunito'>
+                Tu presencia será el mejor regalo para nosotros y para el bebé. ¡Esperamos verte y compartir juntos este día tan especial!
+            </h3>
+            
         </StyledPaper>
     );
 }
